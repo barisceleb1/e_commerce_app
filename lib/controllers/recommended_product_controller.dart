74981c95e19cbfa934c9d1a20476e613c38036c5
barisceleb1 <barisclb71@gmail.com>
2023-01-12 00:15:41 +0300

    e-commerce

--- lib/controllers/recommended_product_controller.dart
+++ lib/controllers/recommended_product_controller.dart
@@ -1,25 +1,26 @@
 import 'package:e_commerce_app/data/repository/populer_product_repo.dart';
+import 'package:e_commerce_app/data/repository/recommended_product_repo.dart';
 import 'package:e_commerce_app/models/products_model.dart';
 import 'package:get/get.dart';
 
 class RecommendedProductController extends GetxController{
-  final RecommendedProductController recommendedProductRepo;
-  PopularProductController({required this.recommendedProductRepo});
+  final RecommendedProductRepo recommendedProductRepo;
+  RecommendedProductController({required this.recommendedProductRepo});
 
-  List<dynamic> _recommendedProductList= [];
-  List<dynamic> get recommendedProductList => _recommendedProductList;
+  List<ProductModel> _recommendedProductList= [];
+  List<ProductModel> get recommendedProductList => _recommendedProductList;
 
   bool _isLoaded = false;
   bool get isLoaded => _isLoaded;
 
-  Future<void> getRecommendProductList()async{
-    Response response = await recommendProductRepo.getRecommendedProductList();
+  Future<void> getRecommendedProductList()async{
+    Response response = await recommendedProductRepo.getRecommendedProductList();
 
     if(response.statusCode == 200){
       print("got products");
       _recommendedProductList=[];
       _recommendedProductList.addAll(Product.fromJson(response.body).products);
-      print(_popularProductList);
+      print(_recommendedProductList);
       _isLoaded= true;
       update();
     }else{
